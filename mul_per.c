ac73326a35a21a6335969b3fec126aaf3ad8dcf3
Lauri Hakko <lauri.hakko@gmail.com>
2021-02-24 07:22:38 +0200

    finished step checker

--- mul_per.c
+++ mul_per.c
@@ -9,6 +9,7 @@ int count_steps(int n) {
     while(n > 9) {
         sprintf(str, "%d", n);
         i = 0;
+        product = 1;
         while(str[i] != 0) {
             product *= (str[i] - '0');
             i++;
@@ -21,7 +22,19 @@ int count_steps(int n) {
 
 int main(){
     int steps = 0;
-    steps = count_steps(29384739);
-    printf("%d\n", steps);
+    long i = 0;
+    int max_steps = 0;
+    int max_i = 0;
+    for(i = 11; i < 3000000000000000; i++) {
+        if (i % 2 == 1) {
+            steps = count_steps(i);
+            if (steps > max_steps) {
+                max_steps = steps;
+                max_i = i;
+                printf("New record: %d %d\n", max_i, max_steps);
+            }
+        }
+    }
+    printf("final record: %d %d\n", max_i, max_steps);
     return 0;
 }
